3bea8d05426cf3d5a79c4da046dcbaa1c45ae8a6
Nemos <christosqwe@yahoo.com>
2021-03-19 17:48:39 +0200

    Update cap.py

--- cap.py
+++ cap.py
@@ -1,5 +1,10 @@
 #sudo chmod +x /usr/bin/dumpcap
 
 import pyshark
+
 cap=pyshark.LiveCapture(interface='eth0')
 cap.sniff(timeout=10)
+
+def print_info(packet):
+  print("PROTOCOL:"+packet.highest_layer+"SOURCE IP:"+packet.ip.src+"DESTINATION:"+packet.ip.dst)
+cap.apply_on_packets(print_info)
